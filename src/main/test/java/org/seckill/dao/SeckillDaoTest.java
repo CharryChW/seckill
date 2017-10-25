89fffde1ea92ea528b58a8cc2b9008a7c9bd9326
charry <wchengcharry@163.com>
2017-10-25 10:26:55 +0800

    create SeckillDaoTest

---
+++ src/main/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,23 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Charry on 2017/10/25.
+ */
+public class SeckillDaoTest {
+    @Test
+    public void reduceNumber() throws Exception {
+    }
+
+    @Test
+    public void queryById() throws Exception {
+    }
+
+    @Test
+    public void queryAll() throws Exception {
+    }
+
+}
