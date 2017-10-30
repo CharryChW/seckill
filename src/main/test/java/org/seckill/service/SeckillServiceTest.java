c0f5fa4fe49e8ac759fc2f83b79cae638dc720e7
charry <wchengcharry@163.com>
2017-10-30 11:26:54 +0800

    添加logback配置

---
+++ src/main/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,43 @@
+package org.seckill.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Charry on 2017/10/30.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+        "classpath:spring/spring-dao.xml",
+        "classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() throws Exception {
+    }
+
+    @Test
+    public void getById() throws Exception {
+    }
+
+    @Test
+    public void exportSeckillUrl() throws Exception {
+    }
+
+    @Test
+    public void executeSeckill() throws Exception {
+    }
+
+}
