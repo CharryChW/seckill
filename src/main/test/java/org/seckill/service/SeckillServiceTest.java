5705c602ec2f88850c17b4d7cc3ff25ed6d5ef12
charry <wchengcharry@163.com>
2017-10-30 11:39:42 +0800

    调整logback配置文件输出级别 开始SeckillService单元测试

--- src/main/test/java/org/seckill/service/SeckillServiceTest.java
+++ src/main/test/java/org/seckill/service/SeckillServiceTest.java
@@ -2,11 +2,14 @@ package org.seckill.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -26,10 +29,15 @@ public class SeckillServiceTest {
 
     @Test
     public void getSeckillList() throws Exception {
+        List<Seckill> list = seckillService.getSeckillList();
+        logger.info("list{}", list);
     }
 
     @Test
     public void getById() throws Exception {
+        long id = 1000;
+        Seckill seckill = seckillService.getById(id);
+        logger.info("seckill={}", seckill);
     }
 
     @Test
